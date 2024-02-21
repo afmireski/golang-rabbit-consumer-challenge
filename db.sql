e5ff4051c8bfcdff8a22f1d86a40eb139ed28d8f
afmireski <afmireski@outlook.com>
2024-02-21 14:00:42 -0300

    Add db.sql

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE
+  `user` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(200) NOT NULL,
+    `name` varchar(200) NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB AUTO_INCREMENT = 37 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
